c872c2025bfc9ba52c6383e7354d56d791f07183
the-datageek <moraaf770@gmail.com>
2023-03-27 08:24:10 +0300

    Added packages for Fontawesome

---
+++ tasks/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import { TodoWrapper } from './components/TodoWrapper';
+//import { Route, Routes} from "react-router-dom";
+
+function App() {
+  return (
+    <div className="App">
+      <TodoWrapper  />
+     
+    </div>
+  
+   
+  );
+}
+
+export default App;
